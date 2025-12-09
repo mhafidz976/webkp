c7fd0a1e1e50deae74fa8170288da8a1ab961806
hafidz muhammad <mhafidz976@gmail.com>
2025-12-09 14:06:27 +0700

    commit 5
    improve ui

--- partials_nav.php
+++ partials_nav.php
@@ -5,6 +5,7 @@ require_once __DIR__ . '/functions.php';
 $user     = current_user();
 $roleKey  = $user['role_key'] ?? '';
 $notifCnt = $user ? unread_notification_count($user['id']) : 0;
+$current  = basename($_SERVER['PHP_SELF'] ?? '');
 ?>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
@@ -14,18 +15,18 @@ $notifCnt = $user ? unread_notification_count($user['id']) : 0;
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item"><a class="nav-link" href="dashboard.php">Dashboard</a></li>
-                <li class="nav-item"><a class="nav-link" href="praktikum_index.php">Jadwal Praktikum</a></li>
-                <li class="nav-item"><a class="nav-link" href="bookings_index.php">Peminjaman Lab</a></li>
-                <li class="nav-item"><a class="nav-link" href="calendar.php">Kalender Lab</a></li>
+                <li class="nav-item"><a class="nav-link<?php echo $current === 'dashboard.php' ? ' active' : ''; ?>" href="dashboard.php">Dashboard</a></li>
+                <li class="nav-item"><a class="nav-link<?php echo $current === 'praktikum_index.php' ? ' active' : ''; ?>" href="praktikum_index.php">Jadwal Praktikum</a></li>
+                <li class="nav-item"><a class="nav-link<?php echo $current === 'bookings_index.php' ? ' active' : ''; ?>" href="bookings_index.php">Peminjaman Lab</a></li>
+                <li class="nav-item"><a class="nav-link<?php echo $current === 'calendar.php' ? ' active' : ''; ?>" href="calendar.php">Kalender Lab</a></li>
                 <?php if (in_array($roleKey, ['admin', 'teknisi'], true)): ?>
-                    <li class="nav-item"><a class="nav-link" href="labs_index.php">Data Lab</a></li>
-                    <li class="nav-item"><a class="nav-link" href="softwares_index.php">Software Lab</a></li>
+                    <li class="nav-item"><a class="nav-link<?php echo $current === 'labs_index.php' ? ' active' : ''; ?>" href="labs_index.php">Data Lab</a></li>
+                    <li class="nav-item"><a class="nav-link<?php echo $current === 'softwares_index.php' ? ' active' : ''; ?>" href="softwares_index.php">Software Lab</a></li>
                 <?php endif; ?>
-                <li class="nav-item"><a class="nav-link" href="tickets_index.php">Tiket Kerusakan</a></li>
+                <li class="nav-item"><a class="nav-link<?php echo $current === 'tickets_index.php' ? ' active' : ''; ?>" href="tickets_index.php">Tiket Kerusakan</a></li>
                 <?php if ($roleKey === 'admin'): ?>
-                    <li class="nav-item"><a class="nav-link" href="users_index.php">Pengguna</a></li>
-                    <li class="nav-item"><a class="nav-link" href="reports.php">Laporan</a></li>
+                    <li class="nav-item"><a class="nav-link<?php echo $current === 'users_index.php' ? ' active' : ''; ?>" href="users_index.php">Pengguna</a></li>
+                    <li class="nav-item"><a class="nav-link<?php echo $current === 'reports.php' ? ' active' : ''; ?>" href="reports.php">Laporan</a></li>
                 <?php endif; ?>
             </ul>
             <div class="d-flex align-items-center">
